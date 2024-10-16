29aa23df39da54785341f1102258ed9a60cad49c
Tang Ziya <tcztzy@gmail.Com>
2024-10-16 11:41:27 +0800

    Add: streaming tool_call for ollama/qwen2.5

--- src/fastoai/routers/ollama/__init__.py
+++ src/fastoai/routers/ollama/__init__.py
@@ -1,16 +1,22 @@
+import json
 from typing import AsyncIterable, cast
 
 from fastapi import Depends
 from fastapi.responses import StreamingResponse
 from openai import AsyncOpenAI
 from openai.types.chat.chat_completion import ChatCompletion
-from openai.types.chat.chat_completion_chunk import ChatCompletionChunk
+from openai.types.chat.chat_completion_chunk import (
+    ChatCompletionChunk,
+    ChoiceDeltaToolCall,
+    ChoiceDeltaToolCallFunction,
+)
 
 from ...models import User, get_current_active_user
 from ...requests import (
     CompletionCreateParams,
 )
 from ...routing import OAIRouter
+from ...utils import random_id_with_prefix
 from ._backend import get_openai
 from .assistants import router as assistants_router
 from .files import router as files_router
@@ -32,14 +38,54 @@ async def create_chat_completions(
 ):
     response = await openai.chat.completions.create(**params.model_dump())
     if params.stream:
+        response = cast(AsyncIterable[ChatCompletionChunk], response)
 
-        async def _stream():
-            async for chunk in cast(AsyncIterable[ChatCompletionChunk], response):
+        async def _stream(chunks: AsyncIterable[ChatCompletionChunk]):
+            async for chunk in chunks:
+                # Hijack the response to add the tool choice because Ollama doesn't support it
+                if (
+                    len(params.tools) > 0
+                    and params.tool_choice != "none"
+                    and params.model.startswith("qwen2.5")
+                    and chunk.choices[0].delta.content == "<tool_call>"
+                ):
+                    new_chunk = chunk.model_copy()
+                    choice = new_chunk.choices[0].model_copy()
+                    choice.finish_reason = "tool_calls"
+                    choice.delta.content = None
+                    tool_call = ""
+                    choice.delta.tool_calls = []
+                    current_index = 0
+                    async for _chunk in chunks:
+                        if _chunk.choices[0].delta.content == "<tool_call>":
+                            tool_call = ""
+                            current_index += 1
+                            continue
+                        if _chunk.choices[0].delta.content == "</tool_call>":
+                            tool_call_json = json.loads(tool_call)
+                            tool_call_json["arguments"] = json.dumps(
+                                tool_call_json["arguments"]
+                            )
+                            choice.delta.tool_calls.append(
+                                ChoiceDeltaToolCall(
+                                    index=current_index,
+                                    id=random_id_with_prefix("call_")(),
+                                    function=ChoiceDeltaToolCallFunction.model_validate(
+                                        tool_call_json
+                                    ),
+                                    type="function",
+                                )
+                            )
+                            continue
+                        tool_call += _chunk.choices[0].delta.content
+                    new_chunk.choices = [choice]
+                    yield f"data: {new_chunk.model_dump_json()}\n\n"
+                    break
                 yield f"data: {chunk.model_dump_json()}\n\n"
-            else:
-                yield "data: [DONE]\n\n"
 
-        return StreamingResponse(_stream())
+            yield "data: [DONE]\n\n"
+
+        return StreamingResponse(_stream(response))
     return cast(ChatCompletion, response)
 
 
